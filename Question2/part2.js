e4459a7873c16b00ff9e907d76a210ae000f7b9a
Sam Tynan <91834747+SamJTynan@users.noreply.github.com>
2022-11-06 23:23:35 +0000

    Update part2.js

--- Question2/part2.js
+++ Question2/part2.js
@@ -1,3 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
-    .then(json => console.log(json.filter(p=>p.title.split("").length>6))) 
+    .then(json => console.log(json.filter(wordcount))) 
+
+function wordcount(json) {
+  const body = json.body
+  const words = body.split(" ")
+  console.log(words.reduce((map, word) => map.set(word, (map.get(word) || 0) + 1), new Map()));
+}
