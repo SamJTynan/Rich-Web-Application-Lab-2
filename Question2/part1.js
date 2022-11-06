8f78e9483c8648256f32a79bc71b69277ee910b8
Sam Tynan <91834747+SamJTynan@users.noreply.github.com>
2022-11-06 22:21:57 +0000

    Update part1.js

--- Question2/part1.js
+++ Question2/part1.js
@@ -1,26 +1,3 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-   var test = Array.from(data);
-  var answer = test.map(myFunction);
-  const results = answer.filter(element => {
-  return element !== undefined;
-  });
-  
-  console.log(results);
-  
-  function myFunction(value) {
-  
-   
-    var myJSON = JSON.stringify(value);
-   	temp1 = myJSON.indexOf('title')+8;
-    temp2 = myJSON.lastIndexOf('body')-3;
-    myJSON = myJSON.slice(temp1,temp2);
-   	const myArray = myJSON.split(" ");
-   	if(myArray.length > 6){
-        return myJSON;
-      }
-}
-
-});
+    .then(response => response.json())
+    .then(json => console.log(json.filter(p=>p.title.split("").length>6))) 
