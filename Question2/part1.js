1c3977db27aecf401316eca214846aaf63061516
Sam Tynan <91834747+SamJTynan@users.noreply.github.com>
2022-11-06 22:22:28 +0000

    Update part1.js

--- Question2/part1.js
+++ Question2/part1.js
@@ -1,3 +1,3 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
-    .then(json => console.log(json.filter(p=>p.title.split("").length>6))) 
+    .then(json => console.log(json.filter(p=>p.title.split(" ").length>6)))
